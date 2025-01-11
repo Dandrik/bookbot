6c6f2b3b74db86283f620fa5a5f3b934173fabc0
Dandrik <nelson.shane@gmail.com>
2025-01-11 12:28:39 -0800

    Printing Alpha by number of times used

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
-
-
-
-
-
-
-
-
-def main():
-    file = "books/frankenstein.txt"
-    text = load_text(file)
-    words = string_to_words(text)
-    count = count_words(words)
-    print(f"{count} words")
+ 
     
 def count_words(words):
     count = 0
     for w in words:
         count += 1
-        print(f"found word {count}")
     return count
 
 def string_to_words(text):
@@ -28,6 +14,43 @@ def load_text(file):
     with open(file) as f:
         text = f.read()
     return text
+
+def count_letters(text):
+    letters = {}
+    for char in text:
+        lowered = char.lower()
+        if lowered in letters:
+            letters[lowered] += 1
+        else:
+            letters[lowered] = 1
+    return letters
+
+def sort_on(d):
+    return d["num"]
+
+def letters_sorted_by_count(letters):
+    sorted_list = []
+    for letter in letters:
+        sorted_list.append({"char": letter, "num":letters[letter]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+        
+def main():
+    file = "books/frankenstein.txt"
+    text = load_text(file)
+    words = string_to_words(text)
+    count = count_words(words)
+    letters = count_letters(text)
+    sorted_letters = letters_sorted_by_count(letters)
+
+    for letter in sorted_letters:
+        if letter["char"].isalpha():
+            print(f"The letter {letter["char"]} occurs {letter["num"]}")
+        else:
+            continue
+ 
+
+
 
 
 main()
